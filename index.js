849cc6e391be0f0e16db2ffd110eefe06daf263e
Arman Hosseini <arman0hosseini@gmail.com>
2024-12-26 17:58:15 +0330

    Operate function created

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// basic math operation functions
+// Basic Math Operation Functions
 function sum(a, b) {
     return a + b;
 };
@@ -12,3 +12,27 @@ function divide(a, b) {
     a / b;
 };
 
+// Operator and Operands Variables
+let firstNumber;
+let operand;
+let secondNumber;
+
+// Operate Function
+function operate(firstNumber, operand, secondNumber) {
+    switch (operand) {
+        case "+":
+            return sum(firstNumber, secondNumber);
+            break;
+        case "-":
+            return subtract(firstNumber, secondNumber);
+            break;
+        case "*":
+            return multipy(firstNumber, secondNumber);
+            break;
+        case "/":
+            return divide(firstNumber, secondNumber);
+            break;
+        default:
+            return "meow";
+    }
+}
