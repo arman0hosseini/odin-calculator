d348a8c648b5168bc4dd1affc757c7409eb2ada1
Arman Hosseini <arman0hosseini@gmail.com>
2025-01-09 01:30:52 +0330

    someHow finished :D

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ function operate(firstNumber, operator, secondNumber) {
             return multipy(firstNumber, secondNumber);
             break;
         case "/":
+            if (secondNumber == 0) {
+                clear();
+                isOkay = false;
+                currentDisplay.textContent = "ERROR";
+                lastDispaly.textContent = "ERROR";
+                return;
+            }
             return divide(firstNumber, secondNumber);
             break;
         default:
@@ -33,11 +40,96 @@ function operate(firstNumber, operator, secondNumber) {
 }
 
 
+//Clear Function
+function clear() {
+    firstNumber = 0;
+    operator = undefined;
+    secondNumber = 0;
+    isFinished = false;
+    isOkay = true;
+    nextNumber = true;
+    currentDisplay.textContent = "0";
+    lastDispaly.textContent = "";
+}
 // Operator and Operands Variables
-let firstNumber = 0;
+let firstNumber;
 let operator;
-let secondNumber = 0;
+let secondNumber;
+let isFinished;
+let isOkay;
+let nextNumber;
+//Nodes
+const currentDisplay = document.querySelector(".current-display");
+const lastDispaly = document.querySelector(".last-display");
+const keys = document.querySelector(".cal-keys");
+
+
+keys.addEventListener("click",
+    function (event) {
+        let target = event.target;
+        if (target.classList.contains("clear")) clear();
+
+        //Number Buttons
+        else if (target.classList.contains("number")) {
+            if (isFinished || isOkay == false) {
+                clear();
+                currentDisplay.textContent = target.value;
+            }
+            else if (nextNumber) {
+                currentDisplay.textContent = target.value;
+                nextNumber = false;
+            }
+            else if (currentDisplay.textContent == "0") {
+                currentDisplay.textContent = target.value;
+            }
+            else {
+                currentDisplay.textContent += target.value;
+            }
+        }
+
+
+        //Operator Buttons
+        else if (target.classList.contains("operator")) {
+            if (isOkay == false) {
+                clear();
+            }
+            else if (isFinished) {
+                isFinished = false;
+                nextNumber = true;
+                operator = target.value;
+                firstNumber = Number(currentDisplay.textContent);
+                lastDispaly.textContent = `${firstNumber} ${operator}`;
+            }
+            else if (operator == undefined) {
+                operator = target.value;
+                firstNumber = Number(currentDisplay.textContent);
+                currentDisplay.textContent = "0"
+                lastDispaly.textContent = `${firstNumber} ${operator}`;
+            }
+        }
+
+
+        //Equal Button
+        else if (target.classList.contains("equal")) {
+            if (isFinished == false && isOkay == true) {
+                secondNumber = Number(currentDisplay.textContent);
+                lastDispaly.textContent = `${firstNumber} ${operator} ${secondNumber} =`;
+                let result = operate(firstNumber, operator, secondNumber);
+                if (isOkay == false) {
+                    isFinished = true
+                    return;
+                }
+                currentDisplay.textContent = result;
+                secondNumber = result;
+                isFinished = true;
+            }
+        }
 
 
 
+    }
+)
 
+
+
+clear();
