e138b92d3e4c3dd4c86f7f86b25e680fa50d4c13
Arman Hosseini <arman0hosseini@gmail.com>
2025-01-12 03:22:16 +0330

    just a step behind

--- index.js
+++ index.js
@@ -38,11 +38,46 @@ function operate(firstNumber, operator, secondNumber) {
     }
 }
 
+
+
 // Display Error Function 
 function displayError() {
     isOkay = false;
     lastDisplay.textContent = "ERROR";
     currentDisplay.textContent = "ERROR";
+}
+
+// Percentage Function
+function percentage(number) {
+    return number / 100;
+}
+
+// Make Negative Number Function
+function makePosOrNeg(number) {
+    if (isNegative) {
+        isNegative = false;
+        return -number;
+    }
+    else {
+        isNegative = true;
+        return -number;
+    }
+}
+
+// Has Decimal Function
+function hasDecimalPoint(number) {
+    if (number % 1 !== 0) {
+        pointUsed = true;
+        return true;
+    }
+}
+
+// Decimal Point Function
+function addDecimalPoint(number) {
+    if (!hasDecimalPoint(number) || !pointUsed) {
+        pointUsed = true;
+        return `${number}.`;
+    }
 }
 
 // Clear Function
@@ -52,6 +87,8 @@ function clear() {
     mainOperator = null;
     isOkay = true;
     nextDigit = false;
+    pointUsed = false;
+    isNegative = false;
     lastDisplay.textContent = "";
     currentDisplay.textContent = "0";
 }
@@ -62,24 +99,53 @@ let secondNumber;
 let mainOperator;
 let isOkay;
 let nextDigit;
+let pointUsed;
+let isNegative;
 
 // Nodes
 const keys = document.querySelector(".cal-keys");
 const lastDisplay = document.querySelector(".last-display");
 const currentDisplay = document.querySelector(".current-display");
 
-
-
+// Click Event Listener
 keys.addEventListener("click",
     function (event) {
         let target = event.target;
 
         if (target.classList.contains("clear")) clear();
 
+        if (target.classList.contains("percentage")) {
+            if (isOkay) {
+                let currentNumber = Number(currentDisplay.textContent);
+                currentDisplay.textContent = percentage(currentNumber);
+            }
+            else clear();
+        }
+
+
+        if (target.classList.contains("pos-neg")) {
+            if (isOkay) {
+                let currentNumber = Number(currentDisplay.textContent);
+                currentDisplay.textContent = makePosOrNeg(currentNumber);
+            }
+            else clear();
+        }
+
+        if (target.classList.contains("point")) {
+            if (isOkay) {
+                if (!pointUsed) {
+                    let currentNumber = Number(currentDisplay.textContent);
+                    currentDisplay.textContent = addDecimalPoint(currentNumber);
+                }
+            }
+            else clear();
+        }
+
         if (target.classList.contains("number")) {
             if (isOkay) {
                 if (!nextDigit || currentDisplay.textContent == "0") {
                     currentDisplay.textContent = target.value;
+                    pointUsed = false;
                     nextDigit = true;
                 }
                 else currentDisplay.textContent += target.value;
@@ -93,6 +159,7 @@ keys.addEventListener("click",
                     firstNumber = Number(currentDisplay.textContent);
                     mainOperator = target.value;
                     currentDisplay.textContent = "0";
+                    pointUsed = false;
                     lastDisplay.textContent = `${firstNumber} ${mainOperator}`;
                     nextDigit = false;
                 }
@@ -100,6 +167,7 @@ keys.addEventListener("click",
                     secondNumber = Number(currentDisplay.textContent);
                     lastDisplay.textContent = `${firstNumber} ${mainOperator} ${secondNumber} =`;
                     let result = operate(firstNumber, mainOperator, secondNumber);
+                    if (!hasDecimalPoint(result)) pointUsed = false;
                     if (!isOkay) {
                         return;
                     }
@@ -119,6 +187,7 @@ keys.addEventListener("click",
                     secondNumber = Number(currentDisplay.textContent);
                     lastDisplay.textContent = `${firstNumber} ${mainOperator} ${secondNumber} =`;
                     let result = operate(firstNumber, mainOperator, secondNumber);
+                    if (!hasDecimalPoint(result)) pointUsed = false;
                     if (!isOkay) {
                         return;
                     }
@@ -132,14 +201,4 @@ keys.addEventListener("click",
         }
     }
 )
-
-
-
-
-
-
-
-
-
-
 clear();
